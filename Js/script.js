9636d9b8aa231d79e440d23a24d4fe53ab7763c6
hruday-dev <thirumalahrudayboda@gmail.com>
2025-10-10 09:24:11 +0530

    Implement email validation and toggle visibility for form and success message

---
+++ Js/script.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('emailform');
+const resetbutton = document.querySelector('.resetbtn');
+const emailInput = document.getElementById('email-input');
+
+const mainpage = document.getElementsByClassName('main-form')[0];
+const messagepage = document.getElementsByClassName('message')[0];
+
+// Create error message <span>
+const errormessage = document.createElement('span');
+errormessage.textContent = "Valid email required";
+errormessage.style.color = 'red';
+errormessage.style.fontSize = '0.90em';
+
+emailInput.onfocus = function() {
+  if (errormessage.parentNode) {
+    errormessage.remove();
+  }
+};
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (errormessage.parentNode) {
+    errormessage.remove();
+  }
+
+  if (!validateEmail(emailInput.value.trim())) {
+    emailInput.before(errormessage);
+  }
+  else {
+
+    mainpage.style.display = 'none';
+    messagepage.style.display = 'flex';
+
+    form.reset();
+  }
+});
+
+resetbutton.addEventListener('click', () => {
+  messagepage.style.display = 'none';
+  mainpage.style.display = 'flex';
+});
